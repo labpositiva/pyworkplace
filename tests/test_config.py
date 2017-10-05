13ba7e0c87b59774aa673e7cbda93725a61124b5
@slovacus <slovacus@gmail.com>
2017-10-04 20:58:35 -0500

    !#26 Fix: settings test config

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-from pyworkplace.config import APP_DEBUG
-from pyworkplace.config import APP_NAME
+from hamcrest import assert_that
+from hamcrest import equal_to
+
+from pyworkplace.config import API_VERSION
 
 
-def test_config():
-    pass
+def test_api_version():
+    assert_that('v2.6', equal_to(API_VERSION))
