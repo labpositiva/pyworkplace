60a0689fbc2220d80673b07c181530a1c0dcbaaf
@slovacus <slovacus@gmail.com>
2017-10-06 18:27:45 -0500

    !#26 Fix: settings vars for config

--- pyworkplace/config.py
+++ pyworkplace/config.py
@@ -7,11 +7,26 @@ DEFAULT_API_VERSION = 'v2.6'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Application
-APP_NAME = os.environ.get('APP_NAME')
-APP_DEBUG = ast.literal_eval(os.environ.get('APP_DEBUG', 'False'))
-WORKPLACE_URL = os.environ.get('WORKPLACE_URL')
-WORKPLACE_VERSION = os.environ.get('WORKPLACE_VERSION')
+
+DEBUG = ast.literal_eval(os.environ.get('PYWORKPLACE_DEBUG', 'False'))
 HEADER_AUTH_KEY = 'Authorization'
 HEADER_AUTH_VAL_PREFIX = 'Bearer'
-ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
-API_VERSION = os.environ.get('API_VERSION', DEFAULT_API_VERSION)
+
+# Workplace
+WORKPLACE_URL = os.environ.get('PYWORKPLACE_URL')
+WORKPLACE_VERSION = os.environ.get('PYWORKPLACE_VERSION')
+WORKPLACE_ACCESS_TOKEN = os.environ.get('PYWORKPLACE_ACCESS_TOKEN')
+WORKPLACE_API_VERSION = os.environ.get(
+    'PYWORKPLACE_API_VERSION', DEFAULT_API_VERSION,
+)
+
+FACEBOOK_GRAPH_VERSION = os.environ.get(
+    'pyworkplace_graph_version',
+    'v2.6',
+)
+FACEBOOK_GRAPH_URL = os.environ.get(
+    'PYWORKPLACE_URL_GRAPH',
+    'https://graph.facebook.com/{}/'.format(
+        FACEBOOK_GRAPH_VERSION,
+    ),
+)
