8708e4e1d7321ff677f8396bd4ca566526305f1c
@slovacus <slovacus@gmail.com>
2017-10-06 17:27:23 -0500

    !#26 Fix: clean config test

--- tests/test_config.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from hamcrest import assert_that
-from hamcrest import equal_to
-
-from pyworkplace.config import API_VERSION
-
-
-def test_api_version():
-    assert_that('v2.6', equal_to(API_VERSION))
