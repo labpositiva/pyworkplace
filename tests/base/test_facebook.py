4ccec2c4c4c513d6ebc3dfca4aa5a470b042f54b
@slovacus <slovacus@gmail.com>
2017-10-07 08:25:15 -0500

    !#26 Add: settings test facebook

--- tests/base/test_facebook.py
+++ tests/base/test_facebook.py
@@ -17,3 +17,21 @@ def test_facebook_default():
         'https://graph.facebook.com/v2.6/',
         equal_to(facebook.url),
     )
+
+
+def test_facebook_change():
+    """ Make instance facebook change
+    """
+    params = {
+        'version': 'v2.8',
+        'access_token': 'this is my token',
+    }
+    facebook = Facebook(**params)
+    assert_that(
+        'v2.8',
+        equal_to(facebook.version),
+    )
+    assert_that(
+        'this is my token',
+        equal_to(facebook.access_token),
+    )
