30c261228c949b62f2f53eff1eb96681e6f68649
@slovacus <slovacus@gmail.com>
2017-10-07 12:18:48 -0500

    !#26 Add: test for messages facebook

---
+++ tests/test_message.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+import json
+
+from hamcrest import assert_that
+from hamcrest import equal_to
+from hamcrest import has_entries
+from hamcrest import not_none
+
+from pyworkplace.message import Message
+
+RECIPIENT_ID = 10000000
+URL = ''
+
+
+def test_send_message():
+    message = Message()
+    body = {
+        'message': 'Hola Mundo',
+        'recipient': {'id': RECIPIENT_ID},
+        'notification_type': 'REGULAR',
+    }
+    args = {
+        'recipient_id': RECIPIENT_ID,
+        'message': 'Hola Mundo',
+    }
+    assert_that(
+        message.send_message(**args),
+        not_none(),
+    )
+
+    response = message.response
+    assert_that(
+        json.loads(response.request.body),
+        has_entries(body),
+    )
+    assert_that(response.request.url, equal_to(URL))
+
+
+def test_send_text_message():
+    message = Message()
+    body = {
+        'message': {
+            'text': 'Hola Mundo',
+        },
+        'recipient': {'id': RECIPIENT_ID},
+        'notification_type': 'REGULAR',
+    }
+    args = {
+        'recipient_id': RECIPIENT_ID,
+        'message': 'Hola Mundo',
+    }
+    assert_that(
+        message.send_text_message(**args),
+        not_none(),
+    )
+
+    response = message.response
+    assert_that(
+        json.loads(response.request.body),
+        has_entries(body),
+    )
+    assert_that(response.request.url, equal_to(URL))
+
+
+def test_send_button_message():
+    message = Message()
+    text = 'Ahora que quieres hacer?'
+    buttons = [
+        {
+            'type': 'postback',
+            'title': 'TERMINAR PEDIDO',
+            'payload': 'PAYLOAD_TERMINAR_PEDIDO',
+        },
+        {
+            'type': 'postback',
+            'title': 'EDITAR PEDIDO',
+            'payload': 'PAYLOAD_EDITAR_PEDIDO',
+        },
+    ]
+    body = {
+        'recipient': {'id': RECIPIENT_ID},
+        'message': {
+            'attachment': {
+                'type': 'template',
+                'payload': {
+                    'template_type': 'button',
+                    'text': text,
+                    'buttons': buttons,
+                },
+            },
+        },
+        'notification_type': 'REGULAR',
+    }
+
+    args = {
+        'recipient_id': RECIPIENT_ID,
+        'text': text,
+        'buttons': buttons,
+    }
+    assert_that(
+        message.send_button_message(**args),
+        not_none(),
+    )
+
+    response = message.response
+    assert_that(
+        json.loads(response.request.body),
+        has_entries(body),
+    )
+    assert_that(response.request.url, equal_to(URL))
