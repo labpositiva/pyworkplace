8232c43be6cbc7a5a2934684c72736ea9fee3d8e
@slovacus <slovacus@gmail.com>
2017-10-07 13:35:48 -0500

    !#26 Add: alias for docker test

--- tests.mk
+++ tests.mk
@@ -2,21 +2,23 @@
 # See ./CONTRIBUTING.rst
 #
 
+DOCKER_TEST := docker-compose -f docker-compose.yml -f docker-compose/test.yml
+
 test.lint: clean
 	pre-commit run --all-files --verbose
 
 test: clean
 	@echo $(MESSAGE) Running tests on the current Python interpreter with coverage $(END)
-	docker-compose -f docker-compose.yml -f docker-compose/test.yml run --rm app \
+	$(DOCKER_TEST) run --rm app \
 		py.test --cov pyworkplace --cov tests --doctest-modules --verbose pyworkplace tests
 	@echo
 
 test.pytest: clean
 	@echo $(MESSAGE) Running tests on the current Python $(END)
 	@if [ "${test}" == "" ]; then \
-		docker-compose -f docker-compose.yml -f docker-compose/test.yml run --rm app \
+		$(DOCKER_TEST) run --rm app \
 			py.test -s pyworkplace tests; \
 	else \
-		docker-compose -f docker-compose.yml -f docker-compose/test.yml run --rm app \
+		$(DOCKER_TEST) run --rm app \
 			py.test -s -v tests/"${test}"; \
 	fi
