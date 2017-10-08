6d7ee2cf0ea44dcd7f64ce75df8ec0a4289ce2ee
@slovacus <slovacus@gmail.com>
2017-10-08 13:49:37 -0500

    !#26 Fix: clean unnecesary button message

--- pyworkplace/message.py
+++ pyworkplace/message.py
@@ -47,29 +47,3 @@ class Message(Facebook):
                 },
             }, notification_type,
         )
-
-    def send_button_message(
-        self, recipient_id, text, buttons,
-        notification_type=NotificationType.regular,
-    ):
-        """Send text messages to the specified recipient.
-        https://developers.facebook.com/docs/messenger-platform/send-api-reference/button-template
-        Input:
-            recipient_id: recipient id to send to
-            text: text of message to send
-            buttons: buttons to send
-        Output:
-            Response from API as <dict>
-        """
-        return self.send_message(
-            recipient_id, {
-                'attachment': {
-                    'type': 'template',
-                    'payload': {
-                        'template_type': 'button',
-                        'text': text,
-                        'buttons': buttons,
-                    },
-                },
-            }, notification_type,
-        )
