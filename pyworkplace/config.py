407b911f9f729708eec09b631ccbc3f223af2dff
@slovacus <slovacus@gmail.com>
2017-10-08 20:55:20 -0500

    !#27 Add: path dir for assets

--- pyworkplace/config.py
+++ pyworkplace/config.py
@@ -4,6 +4,7 @@ import os
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ASSETS_DIR = os.path.join(BASE_DIR, 'tests', 'resources', 'assets')
 # Application
 
 DEBUG = ast.literal_eval(os.environ.get('PYWORKPLACE_DEBUG', 'False'))
