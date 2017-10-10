29f1ee8247f99bcb212e5a767f5efc97f3933b61
@slovacus <slovacus@gmail.com>
2017-10-10 11:42:59 -0500

    !#35 Add: send generic template test

--- tests/message/test_template.py
+++ tests/message/test_template.py
@@ -9,6 +9,98 @@ RECIPIENT_ID = 10000000
 URL = ''
 
 
+def test_send_generic_template():
+
+    params = {
+        'version': 'v2.8',
+        'access_token': 'this is my token',
+    }
+    message = Template(**params)
+
+    args = {
+        'recipient_id': RECIPIENT_ID,
+        'elements': [
+            {
+                'title': 'Title',
+                'image_url': 'https://labpositiva.com/image1.png',
+                'subtitle': 'subtitle.',
+                'default_action': {
+                    'type': 'web_url',
+                    'url': 'https://labpositiva.com/item/103',
+                    'messenger_extensions': True,
+                    'webview_height_ratio': 'tall',
+                    'fallback_url': 'https://labpositiva.com/',
+                },
+                'buttons': [
+                    {
+                        'type': 'web_url',
+                        'url': 'https://url.com',
+                        'title': 'title',
+                    }, {
+                        'type': 'postback',
+                        'title': 'title',
+                        'payload': 'payload',
+                    },
+                ],
+            },
+        ],
+    }
+    assert_that(
+        message.send_generic_message(**args),
+        not_none(),
+    )
+    body = {
+        'message': {
+            'attachment': {
+                'type': 'template',
+                'payload': {
+                    'template_type': 'generic',
+                    'elements': [
+                        {
+                            'title': 'Title',
+                            'image_url': 'https://labpositiva.com/image1.png',
+                            'subtitle': 'subtitle.',
+                            'default_action': {
+                                'type': 'web_url',
+                                'url': 'https://labpositiva.com/item/103',
+                                'messenger_extensions': True,
+                                'webview_height_ratio': 'tall',
+                                'fallback_url': 'https://labpositiva.com/',
+                            },
+                            'buttons': [
+                                {
+                                    'type': 'web_url',
+                                    'url': 'https://url.com',
+                                    'title': 'title',
+                                }, {
+                                    'type': 'postback',
+                                    'title': 'title',
+                                    'payload': 'payload',
+                                },
+                            ],
+                        },
+                    ],
+                },
+            },
+        },
+        'recipient': {'id': RECIPIENT_ID},
+        'notification_type': 'REGULAR',
+
+    }
+
+    response = {
+        'url': 'https://graph.facebook.com/v2.8/me/messages',
+        'params': {'access_token': params['access_token']},
+        'json': body,
+        'headers': {'Content-type': 'application/json'},
+        'method': 'post',
+    }
+    assert_that(
+        response,
+        has_entries(message.response),
+    )
+
+
 def test_send_button():
 
     params = {
