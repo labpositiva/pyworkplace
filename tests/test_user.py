7ad39dc2888bd576a6dc746412c5d2d6469615e1
@slovacus <slovacus@gmail.com>
2017-10-14 17:17:15 -0500

    !#38 Add: test for get_id user

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from hamcrest import assert_that
-from hamcrest import equal_to
 from hamcrest import has_entries
 from hamcrest import not_none
 
 from pyworkplace.user import User
+
+USER_ID = 10000000
 
 
 def test_filter_email():
@@ -23,10 +24,38 @@ def test_filter_email():
         user.filter_by_email(**args),
         not_none(),
     )
-    # 'https://www.facebook.com/scim/v1/Users?filter=userName+eq+"slovacus@gmail.com"
     response = {
         'url': 'https://www.facebook.com/scim/v1/Users',
         'params': {'filter': 'userName+eq+"slovacus@gmail.com"'},
+        'headers': {
+            'Authorization': 'Bearer {}'.format(params['access_token']),
+        },
+        'method': 'get',
+    }
+    assert_that(
+        response,
+        has_entries(user.response),
+    )
+
+
+def test_get_by_id():
+
+    params = {
+        'version': 'v1',
+        'access_token': 'this is my token',
+    }
+    user = User(**params)
+
+    args = {
+        'user_id': USER_ID,
+    }
+
+    assert_that(
+        user.get_by_id(**args),
+        not_none(),
+    )
+    response = {
+        'url': 'https://www.facebook.com/scim/v1/Users/{}'.format(USER_ID),
         'headers': {
             'Authorization': 'Bearer {}'.format(params['access_token']),
         },
