335e4e2a703558f5bc4dfe3d0d082251295690ba
@slovacus <slovacus@gmail.com>
2017-10-15 13:37:58 -0500

    !#39 Add: get all members test with fields

--- tests/graph/test_members.py
+++ tests/graph/test_members.py
@@ -33,3 +33,33 @@ def test_get_all_members_without_fields():
         response,
         has_entries(member.response),
     )
+
+
+def test_get_all_members_with_fields():
+    FIELDS = {
+        'fields': 'id,name',
+    }
+
+    params = {
+        'version': 'v2.8',
+        'access_token': 'this is my token',
+        'community_id': COMMUNITY_ID,
+    }
+    member = Members(**params)
+
+    assert_that(
+        member.get_all_members(fields=FIELDS),
+        not_none(),
+    )
+    response = {
+        'url': 'https://graph.facebook.com/v2.8/{}/members'.format(
+            COMMUNITY_ID,
+        ),
+        'params': FIELDS,
+        'headers': {'access_token': params['access_token']},
+        'method': 'get',
+    }
+    assert_that(
+        response,
+        has_entries(member.response),
+    )
